a4de360482a2ae100f1f512e54a1a76419536a79
Jay <jaydeepkachare123@gmail.com>
2021-12-13 21:24:26 +0530

    Global data added

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,34 @@
   <!-- Header for App -->
   <Header />
 
+  <div class="container">
+  
   <!-- Body -->
   <main class="" v-if="!loading">
+    
     <DataTitle :text="title" :dataDate="dataDate"/>
+
+    <DataBoxes :stats="stats"/>
+  
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching data</div>
     <img :src="loadingImage" alt="loadingImage" class="w-24 m-auto">
   </main>
+  </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import DataTitle from "./components/DataTitle.vue"
+import DataBoxes from "./components/DataBoxes.vue"
 
 export default {
   name: "App",
   components: {
     Header,
-    DataTitle
+    DataTitle,
+    DataBoxes
   },
   data() {
     return {
@@ -45,7 +54,7 @@ export default {
     this.stats = data.Global
     this.countries = data.Countries
     this.loading = false
-
+    console.log(data);
   },
 };
 </script>
